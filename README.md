c9a312a0621ca60e5039bd2c7557059a20414e78
jsson <yx546@cornell.edu>
2023-01-03 01:12:57 -0500

    update auto v2

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # LeetCode Algorithm
 
-| # | Title | Solution |
-|---| ----- | -------- |
-|1|[Two Sum](https://leetcode.com/problems/two-sum/)|[Java](./algorithms/java/1.two-sum.java)|
-|27|[Remove Element](https://leetcode.com/problems/remove-element/)|[Java](./algorithms/java/27.remove-element.java)|
-|704|[Binary Search](https://leetcode.com/problems/binary-search/)|[Java](./algorithms/java/704.binary-search.java)|
+| # | Title | Solution | Difficulty |
+|---| ----- | -------- | -------- |
+|1|[Two Sum](https://leetcode.com/problems/two-sum/)|[Java](./algorithms/java/1.two-sum.java)|Medium|
+|27|[Remove Element](https://leetcode.com/problems/remove-element/)|[Java](./algorithms/java/27.remove-element.java)|Easy|
+|704|[Binary Search](https://leetcode.com/problems/binary-search/)|[Java](./algorithms/java/704.binary-search.java)|Medium|
--- auto_md.py
+++ auto_md.py
@@ -1,4 +1,27 @@
 import os
+import jsonlines
+
+# construct a provblem table
+def json_data_read(json_file_name):
+
+    json_list = []
+    with open(json_file_name, 'r+') as f:
+        for item in jsonlines.Reader(f):
+            json_list.append(item)
+
+    return  json_list
+
+all_problems_path = "/home/jsson/.lc/leetcode/cache/problems.json"
+q_table = json_data_read(all_problems_path)
+q_table = q_table[0]
+
+# get the level of a problem
+def getLevel(q_table, problem):
+    for i in range(len(q_table)):
+        if problem in q_table[i]["link"]:
+            return str(q_table[i]["level"])
+
+# construct a readme file
 readme = "README.md"
 
 if os.path.isfile(readme):
@@ -9,10 +32,8 @@ files =  os.listdir(path)
 files.sort()
 f = open(readme, 'a+')
 f.write("# LeetCode Algorithm" + '\n'+ '\n')
-f.write("| # | Title | Solution |" + '\n')
-f.write("|---| ----- | -------- |" + '\n')
-    
-# |1|[Two Sum]()| [Java](./algorithms/java/1.twoSum.java)|
+f.write("| # | Title | Solution | Difficulty |" + '\n')
+f.write("|---| ----- | -------- | -------- |" + '\n')
 
 # files = str(files).split(".")
 for file in files:
@@ -20,7 +41,7 @@ for file in files:
     file = str(file).split(".")
     if file[2] == "java":
         
-        # part1
+        # part1: problem name
         q_name = file[1]
         q_name = q_name.split("-")
         q_name = [s.capitalize() for s in q_name]
@@ -28,12 +49,15 @@ for file in files:
         url = "https://leetcode.com/problems/" + str(file[1]) + "/"
         part1 = "[" + q_name + "]" + "(" + url + ")"
 
-        # part2
+        # part2: solution path
         q_path = "./algorithms/java/" + str(file_original)
         part2 = "[Java]" + "(" + q_path + ")"
 
-        # output
-        f.write("|" + file[0] + "|" + part1 + "|" + part2 + "|" + '\n')
+        # part3: difficulty
+        part3 = getLevel(q_table, file[1])
+        print(part3)
 
+        # output
+        f.write("|" + file[0] + "|" + part1 + "|" + part2 + "|" + part3 + "|" + '\n')
 
 f.close()
