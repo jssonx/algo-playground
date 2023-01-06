2ba91cbbda9fd34f81d548e3a3931b566085e27e
jsson <yx546@cornell.edu>
2023-01-06 16:28:38 -0500

    update 111

---
+++ algorithms/java/111.minimum-depth-of-binary-tree.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=111 lang=java
+ *
+ * [111] Minimum Depth of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        if (root.left == null) {
+            return right + 1;
+        }
+        if (root.right == null) {
+            return left + 1;
+        }
+        return Math.min(left, right) + 1; 
+    }
+}
+// @lc code=end
+
