cecc5963923da6c5d8af66252095651212bad345
jsn <yx546@cornell.edu>
2023-02-13 20:57:40 -0500

    update 219

---
+++ algorithms/python/219.contains-duplicate-ii.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=219 lang=python3
+#
+# [219] Contains Duplicate II
+#
+
+# @lc code=start
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        s = set()
+        for i, num in enumerate(nums):
+            if i > k:
+                s.remove(nums[i - k - 1])
+            if num in s:
+                return True
+            else:
+                s.add(num)
+        return False
+# @lc code=end
+
