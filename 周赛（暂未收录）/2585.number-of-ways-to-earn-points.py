f442a4c78464a54899040bd8233aacd1866f09de
jsn <yx546@cornell.edu>
2023-03-09 13:22:43 -0500

    update 2585

--- 周赛（暂未收录）/2585.number-of-ways-to-earn-points.py
+++ 周赛（暂未收录）/2585.number-of-ways-to-earn-points.py
@@ -1,14 +1,15 @@
+# dp[i][j]: the number of ways you can earn exactly j points in the exam from tne first i types
 class Solution:
     def waysToReachTarget(self, target: int, types: List[List[int]]) -> int:
-        dp = [[0] * (target + 1) for _ in range(len(types) + 1)]
-        for i in range(len(types) + 1):
+        n = len(types)
+        dp = [[0] * (target + 1) for _ in range(n + 1)]
+        for i in range(n + 1):
             dp[i][0] = 1
-        for i in range(1, len(types) + 1):
-            for j in range(1, target + 1):
-                count = 0
-                for k in range(types[i-1][0] + 1):
-                    if j - k * types[i-1][1] < 0:
+        for i in range(1, n+1):
+            for j in range(1, target+1):
+                for k in range(types[i-1][0]+1):
+                    if k*types[i-1][1] > j:
                         break
-                    count += dp[i-1][j-k*types[i-1][1]]
-                dp[i][j] = count % 1000000007
-        return dp[len(types)][target]
+                    dp[i][j] += dp[i-1][j-k*types[i-1][1]]
+                    dp[i][j] %= 1000000007
+        return dp[n][target]
