69f2843f486fac945e8b8e2513dd215dfa4a0223
jsn <yx546@cornell.edu>
2023-03-11 15:07:46 -0500

    update 145

---
+++ algorithms/python/145.binary-tree-postorder-traversal.py
@@ -0,0 +1,76 @@
+#
+# @lc app=leetcode id=145 lang=python3
+#
+# [145] Binary Tree Postorder Traversal
+#
+# https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+#
+# algorithms
+# Easy (67.42%)
+# Likes:    5541
+# Dislikes: 164
+# Total Accepted:    900.6K
+# Total Submissions: 1.3M
+# Testcase Example:  '[1,null,2,3]'
+#
+# Given the root of a binary tree, return the postorder traversal of its nodes'
+# values.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: root = [1,null,2,3]
+# Output: [3,2,1]
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: root = []
+# Output: []
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: root = [1]
+# Output: [1]
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# The number of the nodes in the tree is in the range [0, 100].
+# -100 <= Node.val <= 100
+# 
+# 
+# 
+# Follow up: Recursive solution is trivial, could you do it iteratively?
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        stk = [root]
+        res = []
+        while stk:
+            node = stk.pop()
+            res.append(node.val)
+            if node.left:
+                stk.append(node.left)
+            if node.right:
+                stk.append(node.right)
+        return res[::-1]
+        
+# @lc code=end
+
