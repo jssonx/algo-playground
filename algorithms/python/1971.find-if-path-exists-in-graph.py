7ad7d93bb3eaf7f06eac61d11144a6487bd36956
jsn <yx546@cornell.edu>
2023-04-04 13:51:31 -0400

    update 1971

--- algorithms/python/1971.find-if-path-exists-in-graph.py
+++ algorithms/python/1971.find-if-path-exists-in-graph.py
@@ -64,6 +64,8 @@
 # @lc code=start
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        if source == destination:
+            return True
         adj_list = [[] for _ in range(n)] 
         for u, v in edges:
             adj_list[u].append(v)
