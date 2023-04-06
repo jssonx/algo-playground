f763770774257d76c7f80320fa2fd8dea3dc28b1
jsn <yx546@cornell.edu>
2023-04-05 23:42:55 -0400

    update 222

--- algorithms/python/222.count-complete-tree-nodes.py
+++ algorithms/python/222.count-complete-tree-nodes.py
@@ -65,11 +65,25 @@
 #         self.right = right
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
+        l, r = root, root
+        hl, hr = 0, 0
+        while l:
+            l = l.left
+            hl += 1
+        while r:
+            r = r.right
+            hr += 1
+        if hl == hr:
+            return 2 ** hl - 1
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+# @lc code=end
+
+'''
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
         if root is None:
             return 0
         left = self.countNodes(root.left)
         right = self.countNodes(root.right)
         return left + right + 1
-        
-# @lc code=end
-
+'''
