29742e0655d91febc035faf2cc9bb73f1d1aa083
jsn <yx546@cornell.edu>
2023-04-07 00:06:11 -0400

    update update_readme

--- PyMDwriter.py
+++ PyMDwriter.py
@@ -190,7 +190,7 @@ for problem in full_prob_table:
 f.write("#### Navigation" + '\n'+ '\n')
 f.write("| " + '\n')
 for tag in sorted(tag_dict, key=lambda x: (-len(tag_dict[x]), x)):
-    f.write("[" + str(tag.capitalize()) + "](#" + str(tag) + ")"+ " `" + str(len(tag_dict[tag])) + "`" + " | ")
+    f.write("[" + str(tag.capitalize()) + "](#" + str(tag) + ")"+ " | ")
 
 f.write("\n")
 
