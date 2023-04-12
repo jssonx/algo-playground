538fbddd7bb09b96dc34657ce625d1bb8781773e
jsn <yx546@cornell.edu>
2023-04-12 16:15:02 -0400

    update 21

--- algorithms/python/21.merge-two-sorted-lists.py
+++ algorithms/python/21.merge-two-sorted-lists.py
@@ -61,16 +61,18 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if list1 is None:
-            return list2
-        if list2 is None:
-            return list1
-        if list1.val < list2.val:
-            list1.next = self.mergeTwoLists(list1.next, list2)
-            return list1
-        else:
-            list2.next = self.mergeTwoLists(list1, list2.next)
-            return list2
+        dummy = ListNode(-1)
+        cur = dummy
+        while list1 and list2:
+            if list1.val <= list2.val:
+                cur.next = list1
+                list1 = list1.next
+            else:
+                cur.next = list2
+                list2 = list2.next
+            cur = cur.next
+        cur.next = list1 if list1 else list2
+        return dummy.next
         
 # @lc code=end
 
