eaa7a2c48f9dbaaa2ec862b7464fc9943dd8988c
jsn <yx546@cornell.edu>
2023-04-12 11:24:02 -0400

    update 20

--- algorithms/python/20.valid-parentheses.py
+++ algorithms/python/20.valid-parentheses.py
@@ -12,7 +12,9 @@ class Solution:
         for c in s:
             if c in pairs:
                 stack.append(c)
-            elif len(stack) == 0 or pairs[stack.pop()]!= c:
+            elif len(stack) == 0:
+                return False
+            elif pairs[stack.pop()] != c:
                 return False
         return len(stack) == 0
 
