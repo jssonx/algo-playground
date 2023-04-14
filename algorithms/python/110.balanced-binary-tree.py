675866fa312680e6c973c762eafb6663e5eb52e0
jsn <yx546@cornell.edu>
2023-04-14 14:30:17 -0400

    update 110

--- algorithms/python/110.balanced-binary-tree.py
+++ algorithms/python/110.balanced-binary-tree.py
@@ -71,3 +71,15 @@ class Solution:
         
 # @lc code=end
 
+'''
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        return abs(self.depth(root.left) - self.depth(root.right)) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right)
+    
+    def depth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return max(self.depth(root.left), self.depth(root.right)) + 1
+'''
